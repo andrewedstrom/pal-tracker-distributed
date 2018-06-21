aeb847a1b285d5f1274fa7072cff71c4f8fa6480
Anoop Gopalakrishnan <agopalakrishnan@pivotal.io>
2018-06-21 15:54:16 -0700

    Revert "Add sso service and local oauth server"
    This reverts commit a20e69167e3cff2c4aea691633d810e0f624fd14.
    
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -1,9 +1,7 @@
 package io.pivotal.pal.tracker.registration;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +21,6 @@ import java.util.TimeZone;
 @EnableEurekaClient
 @EnableWebSecurity
 @EnableResourceServer
-@EnableAutoConfiguration(exclude = LoadBalancerAutoConfiguration.class)
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
